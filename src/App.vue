4187439ccc8309f35f94ea0d9c939af1da5b3835
binnyj <binny923@gmail.com>
2022-02-01 20:07:07 -0500

    spotify authorization

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,70 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      access_token: "",
+      refresh_token: "",
+    };
+  },
+  created() {
+    if (window.location.href.includes("code=")) {
+      let code = null;
+      const queryString = window.location.search;
+      if (queryString) {
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get("code");
+        this.requestAccessToken(code).then((data) => {
+          this.access_token = data.access_token;
+          this.refresh_token = data.refresh_token;
+        });
+      }
+    } else {
+      this.authorizeSpotify();
+    }
+  },
+  methods: {
+    async authorizeSpotify() {
+      // const state = Math.random().toString(16).substr(2, 8);
+      const scope = "user-read-private user-read-email";
+
+      window.location.href =
+        "https://accounts.spotify.com/authorize?" +
+        "response_type=code&" +
+        `client_id=${process.env.VUE_APP_CLIENT_ID}&` +
+        `scope=${scope}&` +
+        `redirect_uri=${encodeURI(process.env.VUE_APP_REDIRECT_URL)}&` +
+        `show_dialog=true`;
+    },
+    async requestAccessToken(code) {
+      const authKey = Buffer.from(
+        process.env.VUE_APP_CLIENT_ID + ":" + process.env.VUE_APP_CLIENT_SEC
+      ).toString("base64");
+
+      const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${authKey}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          grant_type: "authorization_code",
+          code,
+          redirect_uri: process.env.VUE_APP_REDIRECT_URL,
+        }).toString(),
+      });
+
+      return response.json();
+    },
+  },
+};
 </script>
 
 <style>
